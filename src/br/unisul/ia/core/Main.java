641ecf12647e869408e95bfc09136adeb4d63b06
zt-cd <zerodm1@hotmail.com>
2017-06-09 12:24:52 -0300

    clean up main branch from dev-gui

--- src/br/unisul/ia/core/Main.java
+++ src/br/unisul/ia/core/Main.java
@@ -6,9 +6,6 @@ import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		//MainScene window = MainScene.run();
-		
 		boolean success = false;
 		while (!success) {
 			// Square Matrix (AxA)
@@ -25,23 +22,36 @@ public class Main {
 			int option = JOptionPane.showOptionDialog(null, "Robit can exceed maximum energy?", "Options", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
 
 			if (
-				(mazeSize > 0)				&&
-				(maxEnergy > 0)				&&
-				(option > -2 && option < 2)
+                            (mazeSize > 0)		&&
+                            (maxEnergy > 0)		&&
+                            (option > -2 && option < 2)
 			) {
 				success = true;
 				// If Robit can gain more than the maximum energy
 				boolean canExceed;
 				switch (option) {
-					case -1:					return;
-					case 0:	canExceed = true;	break;
-					case 1:	canExceed = false;	break;
-					default:					return;
+					case 0:	canExceed = true; break;
+                                        
+                                        default:
+                                        case -1:
+                                        case 1:	canExceed = false;
 				}
-				Robit robit = new Robit(maxEnergy, canExceed);	// Our ROBIT! :D
-				Maze maze;										// Our MAZE!
-				try { maze = new Maze(robit, mazeSize); } catch (InstantiationException e) { success = false; JOptionPane.showMessageDialog(null, e.getMessage()); continue; }
-				maze.runRobit();								// Make it run like the WIND!
+
+                                // Our ROBIT! :D
+				Robit robit = new Robit(maxEnergy, canExceed);	
+
+                                // Our MAZE!
+				Maze maze; 
+				try {
+                                    maze = new Maze(robit, mazeSize);
+                                } catch (InstantiationException e) {
+                                    success = false;
+                                    JOptionPane.showMessageDialog(null, e.getMessage());
+                                    continue;
+                                }
+
+                                // Make it run like the WIND!
+                                maze.runRobit();
 			} else {
 				JOptionPane.showMessageDialog(null, "ERROR! One or more inputs are not valid!");
 			}
